e889025160e0950192e718447ee5a5496355e058
Peter Chen <13093023233@163.com>
2022-04-05 17:02:33 +0800

    feat: add poem

--- src/services/poems.ts
+++ src/services/poems.ts
@@ -2,14 +2,10 @@ import poems from '@/assets/poems.json';
 import dayjs from 'dayjs';
 import { Poem } from '@/types/poem.type';
 
-const BASE_DATE = dayjs('2022-03-16');
-
-console.log(BASE_DATE);
+const BASE_DATE = dayjs('2022-04-05');
 
 export const getPoem = (): Poem => {
   const dayDiff = dayjs().diff(BASE_DATE, 'day');
 
-  const todayPoem = poems[dayDiff % poems.length];
-
-  return todayPoem;
+  return poems[dayDiff % poems.length];
 };
